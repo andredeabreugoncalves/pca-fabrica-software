0d26986d244e8f4ea7a0088183c14a7d797aa3b1
André de Abreu Gonçalves <70346444+andredeabreugoncalves@users.noreply.github.com>
2022-03-10 13:58:59 -0300

    Update game.js

--- game.js
+++ game.js
@@ -108,7 +108,7 @@ getNewQuestion = () => {
     if (availableQuesions.length === 0 || questionCounter >= MAX_QUESTIONS) {
         localStorage.setItem("mostRecentScore", score);
         //go to the end page
-        return window.location.assign("hhttps://github.com/andredeabreugoncalves/pca-int-app-web/blob/main/end.html");
+        return window.location.assign("https://github.com/andredeabreugoncalves/pca-int-app-web/blob/main/end.html");
     }
     questionCounter++;
     progressText.innerText = `Questionário ${questionCounter}/${MAX_QUESTIONS}`;
